b397e876b971dc7bb274ab7315455ebac3ca805b
knyazyan-lilit <67742030+knyazyan-lilit@users.noreply.github.com>
2021-06-19 17:43:16 +0400

    Add files via upload

--- RandPassGen/RandPassGen.cpp
+++ RandPassGen/RandPassGen.cpp
@@ -1,78 +1,72 @@
 #include <iostream>
 #include <string>
 
+const int rand_range_for_string = 4;
+const int rand_range_for_alphabet = 26;
+
 int random_index_generator()
 {
-
-    int r = rand() % 4;
+    int r = rand() % rand_range_for_string;
     return r;
 }
 
 int alphabet_index_generator()
 {
-
-    int r = rand() % 26;
+    int r = rand() % rand_range_for_alphabet;
     return r;
 }
 
 std::string PasswordGenerator()
 {
-
-    int i = 0;
+    int index = 0;
     std::string Uppercase;
     std::string Lowercase;
     std::string Numbers;
     std::string Symbols;
-
-    while (i < 26)
+    while (index < 26)
     {
-        Uppercase.push_back('A' + i);
-        ++i;
+        Uppercase.push_back('A' + index);
+        ++index;
     }
-
-    i = 0;
-    while (i < 26)
+    index = 0;
+    while (index < 26)
     {
-        Lowercase.push_back('a' + i);
-        ++i;
+        Lowercase.push_back('a' + index);
+        ++index;
     }
-
-    i = 0;
-    while (i < 7)
+    index = 0;
+    while (index < 7)
     {
-        Symbols.push_back('!' + i);
-        ++i;
+        Symbols.push_back('!' + index);
+        ++index;
     }
-    i = 0;
+    index = 0;
     Symbols.push_back(')');
 
-    while (i < 5)
+    while (index < 5)
     {
-        Symbols.push_back('+' + i);
-        ++i;
+        Symbols.push_back('+' + index);
+        ++index;
     }
-    i = 0;
-    while (i < 5)
+    index = 0;
+    while (index < 5)
     {
-        Symbols.push_back(':' + i);
-        ++i;
+        Symbols.push_back(':' + index);
+        ++index;
     }
-    i = 0;
-    while (i < 4)
+    index = 0;
+    while (index < 4)
     {
-        Symbols.push_back('{' + i);
-        ++i;
+        Symbols.push_back('{' + index);
+        ++index;
     }
-
     std::string password;
     int length = 8 + (rand() % (24 - 8 + 1));
     int count_of_elems = 0;
     int count_of_uppercases = 0;
     int count_of_lowercases = 0;
     int count_of_symbols = 0;
-
     password.resize(length);
-
     while (count_of_elems < length)
     {
         int ind = random_index_generator();
@@ -80,7 +74,6 @@ std::string PasswordGenerator()
         {
             continue;
         }
-
         if (count_of_elems == length - 1 && count_of_symbols == 1)
         {
 
@@ -90,34 +83,28 @@ std::string PasswordGenerator()
             count_of_elems++;
             break;
         }
-
         if (count_of_elems == length - 2 && count_of_symbols == 0)
         {
-
             int symbols = rand() % Symbols.size();
             password = password + Symbols[symbols];
             count_of_symbols++;
             count_of_elems++;
-
             symbols = rand() % Symbols.size();
             password = password + Symbols[symbols];
             count_of_symbols++;
             count_of_elems++;
         }
-
         if (ind == 0)
         {
             if ((count_of_uppercases == 2) && (count_of_lowercases == 0 || count_of_lowercases == 1 || count_of_symbols == 0 || count_of_symbols == 1))
             {
                 continue;
             }
-
             int uppercase = alphabet_index_generator();
             password = password + Uppercase[uppercase];
             count_of_uppercases++;
             count_of_elems++;
         }
-
         if (ind == 1)
         {
             if ((count_of_lowercases == 2) && (count_of_uppercases == 0 || count_of_uppercases == 1 || count_of_symbols == 0 || count_of_symbols == 1))
@@ -129,18 +116,14 @@ std::string PasswordGenerator()
             count_of_lowercases++;
             count_of_elems++;
         }
-
         if (ind == 2)
         {
-
             int number = rand() % 10;
             password = password + std::to_string(number);
             count_of_elems++;
         }
-
         if (ind == 3)
         {
-
             int symbols = rand() % Symbols.size();
             password = password + Symbols[symbols];
             count_of_symbols++;
