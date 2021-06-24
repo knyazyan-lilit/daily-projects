4a49cf7e72519a7e9e11b596afae6e88ad628926
knyazyan-lilit <67742030+knyazyan-lilit@users.noreply.github.com>
2021-06-24 13:59:24 +0400

    Add files via upload

---
+++ VirtualFileSystem/VirtualSystem.h
@@ -0,0 +1,32 @@
+#include <vector>
+#include <memory>
+
+struct Node
+{
+    bool state;
+    std::string name;
+    std::vector<Node *> childeren{};
+    Node *parent;
+};
+
+class VirtualSystem
+{
+private:
+    Node *_root;
+    Node *_current;
+    std::string _path;
+
+public:
+    VirtualSystem();
+    void ListContent();
+    void RemoveFolder(const std::string &name);
+    void ChangeToChildDirectory(const std::string &name);
+    void ChangeToParentDirectory();
+    void TouchFile(const std::string &name);
+    void RemoveFile(const std::string &name);
+    void MkDir(const std::string &name);
+    void ShowDirectoryPath();
+    void ChangeDirectoryDoubly(const std::string &name1, const std::string &name2);
+    void DeleteSystem(Node *root);
+    ~VirtualSystem();
+};
